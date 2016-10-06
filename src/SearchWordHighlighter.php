609ca586e67989c70b5cbaf3117e9252981b191a
Marc Neuhaus <marc@mia3.com>
2016-10-07 01:23:04 +0200

    add SearchWordHighlighter

---
+++ src/SearchWordHighlighter.php
@@ -0,0 +1,163 @@
+<?php
+namespace MIA3\Saku;
+
+    /*
+     * This file is part of the mia3/saku package.
+     *
+     * (c) Marc Neuhaus <marc@mia3.com>
+     *
+     * For the full copyright and license information, please view the LICENSE
+     * file that was distributed with this source code.
+     */
+
+/**
+ * Class WrapWordsViewHelper
+ * @package MIA3\Mia3Search\ViewHelpers
+ */
+class SearchWordHighlighter
+{
+
+    protected $string;
+    protected $words = array();
+    protected $wrap = '<strong>|</strong>';
+    protected $crop = null;
+    protected $suffix = '&hellip;';
+    protected $prefix = '&hellip;';
+    protected $wordsBeforeMatch = 10;
+
+    public function __construct($string)
+    {
+        $this->string = $string;
+    }
+
+    /**
+     * @param string $wrap
+     */
+    public function setWrap($wrap)
+    {
+        $this->wrap = $wrap;
+    }
+
+    /**
+     * @param null $crop
+     */
+    public function setCrop($crop)
+    {
+        $this->crop = $crop;
+    }
+
+    /**
+     * @param string $suffix
+     */
+    public function setSuffix($suffix)
+    {
+        $this->suffix = $suffix;
+    }
+
+    /**
+     * @param string $prefix
+     */
+    public function setPrefix($prefix)
+    {
+        $this->prefix = $prefix;
+    }
+
+    /**
+     * @param int $wordsBeforeMatch
+     */
+    public function setWordsBeforeMatch($wordsBeforeMatch)
+    {
+        $this->wordsBeforeMatch = $wordsBeforeMatch;
+    }
+
+    /**
+     *
+     * @param string $string
+     * @param mixed $words
+     * @param string $wrap
+     * @param integer $crop
+     * @param string $suffix
+     * @param string $prefix
+     * @param integer $wordsBeforeMatch
+     * @return string
+     */
+    public function highlight($words)
+    {
+        if (!is_array($words)) {
+            $words = preg_split('/[ ,\.\?]/s', $words);
+        }
+
+        $string = $this->string;
+
+        if ($this->wordsBeforeMatch > 0) {
+            $string = $this->cutBeforeMatch($string, $words);
+        }
+
+        if ($this->crop !== null) {
+            $string = $this->cropWords($string);
+        }
+
+        foreach ($words as $word) {
+            // do a case-insensitive search to find all case-sensitive matches
+            preg_match_all('/(' . preg_quote($word) . ')/i', $string, $matches);
+
+            if (count($matches[0]) > 0) {
+                // replace each found case-sensitive match with it's wrapped
+                // counterpart
+                foreach ($matches[0] as $key => $match) {
+                    $wrappedMatch = str_replace('|', $match, $this->wrap);
+                    $string = str_replace($match, $wrappedMatch, $string);
+                }
+            }
+        }
+
+        return trim($string);
+    }
+
+    public function cutBeforeMatch($content, $words)
+    {
+        $contentWords = preg_split('/[\s]/s', $content);
+        $word = reset($words);
+        foreach ($contentWords as $key => $contentWord) {
+            similar_text(strtolower(trim($contentWord)), strtolower(trim($word)), $match);
+            if ($match > 80) {
+                // if the there are less words before the first search word, we can
+                // break here, because cutting before the first word makes no sense
+                if ($this->wordsBeforeMatch > $key) {
+                    break;
+                }
+
+                // look for the position of the first search word
+                $startPosition = strpos($content, $contentWord);
+
+                for ($i = 1; $i <= $this->wordsBeforeMatch; $i++) {
+                    if (isset($contentWords[$key - $i])) {
+                        $startPosition -= strlen($contentWords[$key - $i]) + 1;
+                    }
+                }
+                $content = $this->prefix . trim(substr($content, $startPosition));
+                break;
+            }
+        }
+
+        return $content;
+    }
+
+    public function cropWords($text)
+    {
+        $text = strip_tags($text);
+
+        $words = preg_split("/[\n\r\t ]+/", $text, $this->crop + 1, PREG_SPLIT_NO_EMPTY);
+        $separator = ' ';
+
+        if (count($words) > $this->crop) {
+            array_pop($words);
+            $text = implode($separator, $words);
+            $text = $text . $this->suffix;
+        } else {
+            $text = implode($separator, $words);
+        }
+
+        return $text;
+    }
+}
---
+++ tests/Unit/SearchWordHighlighterTest.php
@@ -0,0 +1,126 @@
+<?php
+namespace MIA3\Saku\Tests\Unit;
+
+/*
+ * This file is part of the mia3/saku package.
+ *
+ * (c) Marc Neuhaus <marc@mia3.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+use MIA3\Saku\SearchWordHighlighter;
+
+/**
+ * Class SearchWordHighlighterTest
+ * @package MIA3\Saku\Tests\Unit
+ */
+class SearchWordHighlighterTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     * @dataProvider getSomeHighlightingTestValues
+     * @param string $comparison
+     * @param boolean $expected
+     */
+    public function testHighlighting($string, $words, $wrap, $crop, $prefix, $suffix, $wordsBeforeMatch, $expectation) {
+        $highlighter = new SearchWordHighlighter($string);
+        $highlighter->setWrap($wrap);
+        $highlighter->setCrop($crop);
+        $highlighter->setPrefix($prefix);
+        $highlighter->setSuffix($suffix);
+        $highlighter->setWordsBeforeMatch($wordsBeforeMatch);
+
+        $this->assertEquals($highlighter->highlight($words), $expectation);
+    }
+    /**
+     * @return array
+     */
+    public function getSomeHighlightingTestValues() {
+        return array(
+            array(
+                'string' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi deleniti dolore esseeveniet excepturi ipsum iusto.',
+                'words' => "lorem amet",
+                'wrap' => '<b>|</b>',
+                'crop' => null,
+                'prefix' => null,
+                'suffix' => null,
+                'wordsBeforeMatch' => null,
+                'expecation' => '<b>Lorem</b> ipsum dolor sit <b>amet</b>, consectetur adipisicing elit. Accusamus animi deleniti dolore esseeveniet excepturi ipsum iusto.'
+            ),
+            array(
+                'string' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi deleniti dolore esseeveniet excepturi ipsum iusto.',
+                'words' => "deleniti",
+                'wrap' => '<b>|</b>',
+                'crop' => null,
+                'prefix' => null,
+                'suffix' => null,
+                'wordsBeforeMatch' => null,
+                'expecation' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi <b>deleniti</b> dolore esseeveniet excepturi ipsum iusto.'
+            ),
+            array(
+                'string' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi deleniti dolore esseeveniet excepturi ipsum iusto.',
+                'words' => "deleniti",
+                'wrap' => '<b>|</b>',
+                'crop' => null,
+                'prefix' => null,
+                'suffix' => null,
+                'wordsBeforeMatch' => 3,
+                'expecation' => 'elit. Accusamus animi <b>deleniti</b> dolore esseeveniet excepturi ipsum iusto.'
+            ),
+            array(
+                'string' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi deleniti dolore esseeveniet excepturi ipsum iusto.',
+                'words' => "deleniti ipsum",
+                'wrap' => '<b>|</b>',
+                'crop' => null,
+                'prefix' => null,
+                'suffix' => null,
+                'wordsBeforeMatch' => 3,
+                'expecation' => 'elit. Accusamus animi <b>deleniti</b> dolore esseeveniet excepturi <b>ipsum</b> iusto.'
+            ),
+            array(
+                'string' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi deleniti dolore esseeveniet excepturi ipsum iusto.',
+                'words' => "deleniti ipsum",
+                'wrap' => '<b>|</b>',
+                'crop' => 6,
+                'prefix' => null,
+                'suffix' => null,
+                'wordsBeforeMatch' => 3,
+                'expecation' => 'elit. Accusamus animi <b>deleniti</b> dolore esseeveniet'
+            ),
+            array(
+                'string' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi deleniti dolore esseeveniet excepturi ipsum iusto.',
+                'words' => "deleniti ipsum",
+                'wrap' => '<b>|</b>',
+                'crop' => 6,
+                'prefix' => '...',
+                'suffix' => null,
+                'wordsBeforeMatch' => 3,
+                'expecation' => '...elit. Accusamus animi <b>deleniti</b> dolore esseeveniet'
+            ),
+            array(
+                'string' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus animi deleniti dolore esseeveniet excepturi ipsum iusto.',
+                'words' => "deleniti ipsum",
+                'wrap' => '<b>|</b>',
+                'crop' => 6,
+                'prefix' => '...',
+                'suffix' => '...',
+                'wordsBeforeMatch' => 3,
+                'expecation' => '...elit. Accusamus animi <b>deleniti</b> dolore esseeveniet...'
+            ),
+            array(
+                'string' => 'Lorem ipsum dolor sit amet, consectetur adipisicing 
+                
+                elit. Accusamus animi deleniti dolore esseeveniet        excepturi ipsum iusto.',
+                'words' => "deleniti ipsum",
+                'wrap' => '<b>|</b>',
+                'crop' => 6,
+                'prefix' => '...',
+                'suffix' => '...',
+                'wordsBeforeMatch' => 3,
+                'expecation' => '...elit. Accusamus animi <b>deleniti</b> dolore esseeveniet...'
+            )
+        );
+    }
+}
